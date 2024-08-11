43448dc7b83a302ee2c0b88b522771b3e9da80b9
Umakant1901 <ukp.linux@gmail.com>
2024-08-11 12:39:07 +0530

    basic set up done for LeetCode practice

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        System.out.println("Welcome to Java LeetCode practice");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
